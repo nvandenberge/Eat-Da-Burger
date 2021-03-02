4be5483a03dfd260cd027db1219ffbea20b7504b
Nick Vandenberge <nvandenberge14@gmail.com>
2021-03-02 18:14:30 -0500

    updated database connection for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let db;
 
-const db = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "P@ssw0rdP@ssw0rd",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+  db = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  db = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "P@ssw0rdP@ssw0rd",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 db.connect(function(err) {
